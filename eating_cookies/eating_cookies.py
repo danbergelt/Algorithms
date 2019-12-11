d0823d41fcf020138cc6afed1048e65fec598101
Dan <danbergelt@protonmail.com>
2019-12-11 17:42:04 -0500

    mvp done

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,23 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+
+# preset cache with limits (stops at 2 since eating_cookies(3) == 4)
+def eating_cookies(n, cache={0:1, 1:1, 2:2}):
+  # if n is not in cache, recursively call eating_cookies to generate permutation for each amount of cookies at once
+  if n not in cache:
+    cache[n] = eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
+  return cache[n]
+
+"""
+e.g. fn(5) == 13 or 2 + 4 + 7
+
+2 + 3           +            4
+   ^^^                      ^^^
+(1 + 1 + 2 = 4)         (1 + 2 + 3 = 7)
+                                ^^^
+                          (1 + 2 + 2 = 4)
+"""
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
