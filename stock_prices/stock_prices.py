f0c44cd75e27e9ece88074a48eca0d3ca7183ec2
Dan <danbergelt@protonmail.com>
2019-12-11 15:34:53 -0500

    stock prices done

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python
 
 import argparse
+import math
 
 def find_max_profit(prices):
-  pass
+
+  # instantiate a max difference (i.e. final answer) 
+  # and a smallest number in list to perform comparisons
+  maxDiff = -math.inf
+  minNumber = prices[0]
+
+  for i in range(0, len(prices) - 1):
+    # if the current price in loop is less than the smallest number,
+    # set that price to be the smallest number
+    if prices[i] < minNumber:
+      minNumber = prices[i]
+
+    # if the price one slot ahead (to simulate a buy/sell) is greater than
+    # the current max diff, that's the new max diff
+    if prices[i + 1] - minNumber > maxDiff:
+      maxDiff = prices[i + 1] - minNumber
+  
+  return maxDiff
+
+  
+
 
 
 if __name__ == '__main__':
