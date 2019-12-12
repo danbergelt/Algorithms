85b42c8db97ffaa7813a55af8fdc4295cff01ea5
Dan <danbergelt@protonmail.com>
2019-12-11 23:37:28 -0500

    rps done

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,35 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
 
+  # potential moves in rps
+  moves = ["rock", "paper", "scissors"]
+  
+  # container for full list of plays
+  games = []
+
+  # recursive helper function to find combinations of plays
+  def helper(gamesPlayed, rounds):
+    nonlocal moves
+    nonlocal games
+
+    # base case - exits the loop if the rounds in this iteration have exhausted
+    if (rounds == 0):
+      games.append(gamesPlayed)
+      return
+
+    for move in moves:
+      helper(gamesPlayed + [move], rounds - 1)
+  
+    """
+      e.g. n == 2
+      layer one:      ['r'],           ['p'],              ['s']
+      layer two:        ^                ^                   ^
+                    [r] [p] [s]     [r] [p] [s]         [r] [p] [s] 
+    """  
+  helper([], n)
+
+  return games
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
